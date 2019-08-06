8acbd50dc57d28717ac4d3ddbba6369418ba092a
dberghammer <BerghammerDomi@gmail.com>
2019-08-06 20:27:13 +0200

    parameter input for network device, list ip and icmp packets

--- PacketManipulation/main.go
+++ PacketManipulation/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"github.com/google/gopacket/layers"
 	"fmt"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/pcap"
 	"log"
 	"time"
+	"os"
 )
 
 var (
@@ -18,6 +20,10 @@ var (
 )
 
 func main() {
+	if len(os.Args) > 1 {
+		device = os.Args[1]
+	}
+
 	handle, err = pcap.OpenLive(device, snaplen, promisc, timeout)
 	if err != nil {
 		log.Fatal(err)
@@ -35,21 +41,41 @@ func main() {
 	for packet := range packetsource.Packets() {
 		fmt.Println("Ping:")
 		fmt.Println(packet)
+
+		fmt.Println("---------------")
+
+		ipLayer := packet.Layer(layers.LayerTypeIPv4)
+		ipPacket, _ := ipLayer.(*layers.IPv4)
+		icmpLayer := packet.Layer(layers.LayerTypeICMPv4)
+		icmpPacket := icmpLayer.(*layers.ICMPv4)
+
+		fmt.Println("Source: " + ipPacket.SrcIP.String())
+		fmt.Println("Destination: " + ipPacket.DstIP.String())
+		fmt.Println("Protocol: ", ipPacket.Protocol)
+		fmt.Println("Source: " + ipPacket.SrcIP.String())
+
+		fmt.Println("---------------")
+
+		fmt.Println("ICMP Code: ", icmpPacket.TypeCode)
+		fmt.Println("ICMP Sequence Nr: ", icmpPacket.Seq)
+		fmt.Println("Payload length: ", len(icmpPacket.Payload))
+		fmt.Println("Payload data: ", icmpPacket.Payload)
+		fmt.Println("Payload data to string: ", string(icmpPacket.Payload))
 	}
 
 	// devices, err := pcap.FindAllDevs()
-    // if err != nil {
-    //     log.Fatal(err)
-    // }
+	// if err != nil {
+	//     log.Fatal(err)
+	// }
 
-    // fmt.Println("Devices found:")
-    // for _, device := range devices {
-    //     fmt.Println("\nName: ", device.Name)
-    //     fmt.Println("Description: ", device.Description)
-    //     fmt.Println("Devices addresses: ", device.Description)
-    //     for _, address := range device.Addresses {
-    //         fmt.Println("- IP address: ", address.IP)
-    //         fmt.Println("- Subnet mask: ", address.Netmask)
-    //     }
-    // }
+	// fmt.Println("Devices found:")
+	// for _, device := range devices {
+	//     fmt.Println("\nName: ", device.Name)
+	//     fmt.Println("Description: ", device.Description)
+	//     fmt.Println("Devices addresses: ", device.Description)
+	//     for _, address := range device.Addresses {
+	//         fmt.Println("- IP address: ", address.IP)
+	//         fmt.Println("- Subnet mask: ", address.Netmask)
+	//     }
+	// }
 }
